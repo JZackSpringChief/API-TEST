f015a3c97a3fd2aaec8a830a59fdb7e4fda8c9d2
Zack Spring Chief <jzacksc@gmail.com>
2023-08-25 23:13:55 -0600

    .pop and .push methods with .length

--- h.js
+++ h.js
@@ -35,3 +35,32 @@ const friends = ["Jane", "Jim", "Jeri"];
 for (let i = 0; i < friends.length; i++) {
   console.log(friends[i]);
 }
+
+const classes = ["math", "physics", "geography"];
+const arrayLength = classes.length;
+console.log(arrayLength);
+
+const counter = [1, 2, 3, 4, 5, 6];
+counter.pop();
+console.log(counter.length);
+
+const weather = ["sunny", "rainy", "cloudy"];
+for (let i = 0; i < weather.length; i++) {
+  console.log(weather[i]);
+}
+
+const siblings = ["Jon", "Jess", "Jen", "Jarod"];
+const add = siblings.push("Jacob");
+const middleChild = siblings[2];
+console.log(`The middle child is ${middleChild}.`);
+
+// Let's create an array that holds a bunch of to-do items.
+// 1. Code an array that contains a list of these to-do's:  "⁠s⁠t⁠u⁠d⁠y⁠"⁠ ,  "⁠c⁠h⁠o⁠r⁠e⁠s⁠"⁠ , and  "⁠m⁠e⁠d⁠i⁠t⁠a⁠t⁠e⁠"⁠ . Store the array in the  t⁠o⁠d⁠o⁠s⁠  variable.
+// 2. Use  p⁠o⁠p⁠  to remove the final to-do from the array and store this in the  r⁠e⁠m⁠o⁠v⁠e⁠  variable.
+// 3. Create a  c⁠o⁠n⁠s⁠t⁠  variable  l⁠e⁠f⁠t⁠T⁠o⁠D⁠o⁠  that stores the number of items left in the  t⁠o⁠d⁠o⁠s⁠  array. Use an array property to code this step.
+// Output:
+
+const todos = ["study", "chores", "meditate"];
+const remove = todos.pop();
+const leftToDo = todos.length;
+console.log(`You have ${leftToDo} items left to do.`);
