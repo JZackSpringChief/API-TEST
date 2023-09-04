2649ea149ef073e82b210a89e7b578e3a6fa2009
Zack Spring Chief <jzacksc@gmail.com>
2023-09-04 17:17:51 -0600

    adding img changer

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 // const museum = document.getElementById("museum");
 // museum.innerHTML = "MoMA";
 
-function save() {
-  const el = document.querySelector("span");
-  el.innerHTML = "saved";
+// function save() {
+//   const el = document.querySelector("span");
+//   el.innerHTML = "saved";
+// }
+
+function show() {
+  const el = document.getElementById("dessert");
+  el.setAttribute("src", "https://mimo.app/i/croissant.png");
 }
