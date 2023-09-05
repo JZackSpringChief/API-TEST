ec8660fd3223a50e23597e253c28c24e645fccf7
Zack Spring Chief <jzacksc@gmail.com>
2023-09-04 20:25:37 -0600

    need to study more dynamic html

--- script.js
+++ script.js
@@ -98,3 +98,10 @@ const sendReview = () => {
   console.log(reviewInput);
   console.log(reviewTextarea);
 };
+
+function nextLocation() {
+  const text = document.querySelector("h1");
+  text.innerHTML = "Travel to the Moon";
+  const el = document.querySelector("img");
+  el.src = "https://mimo.app/i/moon.png";
+}
