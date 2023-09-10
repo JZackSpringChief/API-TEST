4fff97fa5f692500fac205676e44b1f1be973c10
Zack Spring Chief <jzacksc@gmail.com>
2023-09-10 15:42:30 -0600

    testing

---
+++ rpg.js
@@ -0,0 +1,29 @@
+// class PlayerOne {
+//   constructor(hitPoints, attack, defense) {
+//     this.hitPoints = hitPoints;
+//     this.attack = attack;
+//     this.defense = defense;
+//   }
+//   attack()
+//   attack = 10
+// }
+
+// class Warrior extends PlayerOne {
+//   constructor() {
+//     console.log("hello");
+//   }
+//   attack()
+//   super.attack()
+// }
+
+// const newWarrior = new Warrior();
+
+// console.log(newWarrior);
+
+const addNum = (a) => {
+  for (i = 0; i < a; i++) {
+    console.log(i);
+  }
+};
+
+console.log(addNum(10));
