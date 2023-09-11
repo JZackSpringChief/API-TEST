d920cf7496ef82dde037f67a498f3243a68eabbe
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 15:25:44 -0600

    !=

--- index.jsx
+++ index.jsx
@@ -1,40 +1,40 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Questionnaire = (props) => {
-  const [first, setFirst] = useState("");
-  const [second, setSecond] = useState("");
+const CreateAccount = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const updateFirst = (e) => {
-    setFirst(e.target.value);
-  };
-  const updateSecond = (e) => {
-    setSecond(e.target.value);
+  const handleInput = (e) => {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
   };
 
-  let ending;
-  if (first.length > 0 && second.length < 0) {
-    ending = <button>Submit Form</button>;
-  } else {
-    ending = <p>Please answer all the questions above.</p>;
+  if (props.user != null) {
+    return <p>You are already logged in!</p>;
   }
 
   return (
     <div>
-      <h1>Boogle Forms</h1>
-      <p>
-        <strong>What is your favorite computer program/app?</strong>
-      </p>
-      <input name="first" type="text" value={first} onChange={updateFirst} />
-      <p>
-        <strong>What computer model do you use?</strong>
-      </p>
-      <input name="second" type="text" value={second} onChange={updateSecond} />
-      {ending}
+      <p>Email:</p>
+      <input type="email" name="email" value={email} onChange={handleInput} />
+      <p>Password:</p>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={handleInput}
+      />
     </div>
   );
 };
 
-ReactDOM.render(<Questionnaire />, document.getElementById("root"));
+ReactDOM.render(
+  <CreateAccount user={{ username: "MeIsJohnDoe" }} />,
+  document.getElementById("root")
+);
 
-export default Questionnaire;
+export default CreateAccount;
