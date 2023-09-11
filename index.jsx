299d5ec204dc5f661935632422288c704f57d107
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 13:38:51 -0600

    fixing if

--- index.jsx
+++ index.jsx
@@ -1,22 +1,59 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-const Validity = (props) => {
-  const [valid, isValid] = useState(false);
+const App = () => {
+  const [on, setOn] = useState(true);
+  const [count, setCount] = useState(50);
 
   useEffect(() => {
-    if (props.user != null && props.user.isAuthenticated) isValid(true);
-    else isValid(false);
-  }, []);
+    const timeout = setTimeout(() => {
+      if (count > 0 && on) setCount((c) => c - 1);
+    }, 25);
+    return () => {
+      if (timeout != null) clearTimeout(timeout);
+    };
+  }, [count]);
 
-  return <div>{valid ? props.children : <p>This is an invalid user</p>}</div>;
+  const cancelCountdown = () => {
+    setOn(false);
+  };
+  const resetCountdown = () => {
+    setOn(true);
+    setCount(50);
+  };
+
+  const content =
+    count > 0 ? (
+      on ? (
+        <button name="hurry" onClick={cancelCountdown}>
+          Hurry, click me!
+        </button>
+      ) : (
+        <div>
+          <p>You win!</p>
+          <button name="reset_win" onClick={resetCountdown}>
+            Reset Game
+          </button>
+        </div>
+      )
+    ) : (
+      <div>
+        <p>Oh no, game over...</p>
+        <button name="reset_lose" onClick={resetCountdown}>
+          Reset Game
+        </button>
+      </div>
+    );
+
+  return (
+    <div>
+      <h1>Click the button before it counts down to 0!</h1>
+      <h3>{count}</h3>
+      {content}
+    </div>
+  );
 };
 
-ReactDOM.render(
-  <Validity user={{ name: "Jill", isAuthenticated: true }}>
-    <h3>Hello, Jill</h3>
-  </Validity>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-export default Validity;
+export default App;
