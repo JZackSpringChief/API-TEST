e519f69184b3dc5e6ea4d7979d25b6cce33a80d8
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 15:19:13 -0600

    react advanced

--- index.jsx
+++ index.jsx
@@ -1,48 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-const NameForm = () => {
-  //Add code below
-  const [firstname, setFirst] = useState("");
-  const [lastname, setLast] = useState("");
+const Goal = (props) => {
+  var announcement;
 
-  const handleFirst = (event) => {
-    setFirst(event.target.value);
-  };
-  const handleSecond = (event) => {
-    setLast(event.target.value);
-  };
-
-  return (
-    <div className="form">
-      <h1>Your Credentials</h1>
-      <div className="form_content">
-        <p>
-          <strong>
-            Please tell us your first and last name, in that order.
-          </strong>
-        </p>
-        <div className="form_row">
-          <input
-            type="text"
-            name="first"
-            value={firstname}
-            onChange={handleFirst}
-            placeholder="First Name"
-          />
-          <input
-            type="text"
-            name="last"
-            value={lastname}
-            onChange={handleSecond}
-            placeholder="Last Name"
-          />
-        </div>
+  if (props.isGoal === true) {
+    announcement = (
+      <div>
+        <h1>Goal!</h1>
+        <p>It's a goal!</p>
       </div>
-    </div>
-  );
+    );
+  } else {
+    announcement = (
+      <div>
+        <h1>Miss</h1>
+        <p>Doh! It's a miss...</p>
+      </div>
+    );
+  }
+
+  return announcement;
 };
 
-ReactDOM.render(<NameForm />, document.getElementById("root"));
+ReactDOM.render(<Goal isGoal={true} />, document.getElementById("root"));
 
-export default NameForm;
+export default Goal;
