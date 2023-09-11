05b49d6534fde5de0d80da2e183cd8e3d1f982ce
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 11:49:48 -0600

    react components

--- index.jsx
+++ index.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Todo } from "./Todo";
 
-export const App = () => {
-  return (
-    <div>
-      <h1>To Do List</h1>
-      <h2>First Task:</h2>
-      <Todo title="Study" />
-    </div>
-  );
-};
+const content = <h1>My first react content</h1>;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(document.getElementById("root"));
+
+export default content;
