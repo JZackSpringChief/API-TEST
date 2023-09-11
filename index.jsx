c20301d8f869fa1cbe044b6016da9d08447da1c8
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 13:40:10 -0600

    fixing add count

--- index.jsx
+++ index.jsx
@@ -1,59 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const [on, setOn] = useState(true);
-  const [count, setCount] = useState(50);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (count > 0 && on) setCount((c) => c - 1);
-    }, 25);
-    return () => {
-      if (timeout != null) clearTimeout(timeout);
-    };
-  }, [count]);
-
-  const cancelCountdown = () => {
-    setOn(false);
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const AddCount = () => {
+    setCount(count + 1);
   };
-  const resetCountdown = () => {
-    setOn(true);
-    setCount(50);
+  const SubtractCount = () => {
+    setCount(count - 1);
   };
-
-  const content =
-    count > 0 ? (
-      on ? (
-        <button name="hurry" onClick={cancelCountdown}>
-          Hurry, click me!
+  return (
+    <div className="counter">
+      <div className="count">
+        <h1>{count}</h1>
+      </div>
+      <div className="buttons">
+        <button name="add" onClick={AddCount}>
+          +
         </button>
-      ) : (
-        <div>
-          <p>You win!</p>
-          <button name="reset_win" onClick={resetCountdown}>
-            Reset Game
-          </button>
-        </div>
-      )
-    ) : (
-      <div>
-        <p>Oh no, game over...</p>
-        <button name="reset_lose" onClick={resetCountdown}>
-          Reset Game
+        <button name="subtract" onClick={SubtractCount}>
+          -
         </button>
       </div>
-    );
-
-  return (
-    <div>
-      <h1>Click the button before it counts down to 0!</h1>
-      <h3>{count}</h3>
-      {content}
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
-export default App;
+export default Counter;
