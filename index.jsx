386a5e9c99cd26588b6e1f68114616539d17e1fc
Zack Spring Chief <jzacksc@gmail.com>
2023-09-11 11:36:32 -0600

    adding and testing props

--- index.jsx
+++ index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 
-class CarBrands extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { makes: ["Volkswagen", "Ford", "Honda"] };
-  }
-
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.makes.map((make, i) => {
-            return <li key={"make-" + i}>{make}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+export const Todo = (props) => {
+  return (
+    <div>
+      <h3>{}</h3>
+      <p>This is some generic description text</p>
+    </div>
+  );
+};
