550d4e2a7a553a8b527f3d1794c1657690fe6e0c
Zack Spring Chief <jzacksc@gmail.com>
2023-09-18 15:40:50 -0600

    end points for movies

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get("/", (request, response) => {
   response.status(200).send("<p>if you see this it a good ole 200 status</p>");
 });
 
+app.get("/movies", (req, res) => {
+  res.send("You are in the movie directory");
+});
+
+app.get("/movies/:id", (req, res) => {
+  res.send(`im a movie id in id ${req.params.id} in the movie directory`);
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
