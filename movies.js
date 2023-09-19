e5d6046789a41e1165758df8fdd54ea718092e93
Zack Spring Chief <jzacksc@gmail.com>
2023-09-19 12:11:57 -0600

    added extention

--- movies.js
+++ movies.js
@@ -2,12 +2,14 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("You are in the movie directory");
+  res.status(200).send("You are in the movie directory");
 });
 
 router.get("/:id/:name", (req, res) => {
   const { id, name } = req.params;
-  res.send(`im a movie id in id ${id} and the ${name} in the movie directory`);
+  res
+    .status(200)
+    .send(`im a movie id in id ${id} and the ${name} in the movie directory`);
 });
 
 export default router;
